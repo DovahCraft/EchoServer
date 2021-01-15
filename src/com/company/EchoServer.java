4f35d4fb0831a9ba40be87ae80d2944400860556
Jared Weinberger <jared.j.weinberger@gmail.com>
2021-01-15 13:52:21 -0700

    Finished server, still needs cleanup

--- src/com/company/EchoServer.java
+++ src/com/company/EchoServer.java
@@ -1,12 +1,11 @@
 package com.company;
-import java.util.regex.*;
+
 import java.net.*;
 import java.io.*;
 
-
 public class EchoServer {
-    public static void main(String[] args){
-        if(args.length != 1){
+    public static void main(String[] args) {
+        if (args.length != 1) {
             System.err.println("Please supply a port number only to run the server.");
             System.exit(1);
         }
@@ -14,21 +13,19 @@ public class EchoServer {
         int portNumber = Integer.parseInt(args[0]);
         System.out.println("Running server");
 
-        try(ServerSocket serverSocket = new ServerSocket(portNumber))
-            {
-                //Accept new client connections
-                while(true){
-                    System.out.println("Client connected!");
-                    Thread echoThread = new Thread(new EchoThread(serverSocket.accept()));
-                    echoThread.start();
-                }
-
-        }
-        catch (IOException e){
-            System.out.println("Error when tyring to listen on port "+ portNumber);
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            //Accept new client connections
+            Socket newClient;
+            while (true) {
+                newClient = serverSocket.accept(); // Waits until a client connects
+                System.out.println("Client connected!");
+                EchoThread echoThread = new EchoThread(newClient);
+                new Thread(echoThread).start(); // Handles that new client
+            }
+        } catch (IOException e) {
+            System.out.println("Error when trying to listen on port " + portNumber);
             System.out.println(e.getMessage());
         }
-
 
 
     }
--- src/com/company/EchoThread.java
+++ src/com/company/EchoThread.java
@@ -1,78 +1,57 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class EchoThread implements Runnable{
+public class EchoThread implements Runnable {
+    final String terminationWord = "quit";
     Socket clientSocket;
-    public EchoThread(Socket inputSocket){
+
+    public EchoThread(Socket inputSocket) {
         clientSocket = inputSocket;
     }
+
     public void run() {
-        //Testing function for the regex for quit.
-        testRegex();
-        try(
-        PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
-        InputStreamReader fromClient = new InputStreamReader(clientSocket.getInputStream());
-        )
-        {
+        Socket clientSocket = this.clientSocket;
+        try (PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
+             InputStreamReader fromClient = new InputStreamReader(clientSocket.getInputStream());
+             clientSocket) {
             int inputByte;
             char inputChar;
-            while ((inputByte = fromClient.read()) != -1) {
+            int letterIndex = 0;
+            // While the connection is open and the entire termination sequence hasn't been received
+            while (letterIndex < 4 && (inputByte = fromClient.read()) != -1) {
                 inputChar = (char) inputByte;
                 //Check char type, if it is uppercase/lowercase english letter,
-                if(isEnglishLetter(inputChar))
-                    toClient.println("Valid char inputted: " + inputChar);
-                //Otherwise dont
+                // Ignore if not English letter
+                if (isEnglishLetter(inputChar)) {
+                    toClient.println(inputChar);
+                    // if the letter is in the sequence
+                    if (terminationWord.charAt(letterIndex) == inputChar) {
+                        letterIndex++; // look for the next letter in the sequence
+                    }
+                    // if we get 'q', it is the start of a new sequence.
+                    else if (terminationWord.charAt(0) == inputChar) {
+                        letterIndex = 1; // second letter, look for 'u'
+                    }
+                    // else reset
+                    else {
+                        letterIndex = 0; // look for 'q'
+                    }
+                }
 
             }
 
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Connection Closed");
     }
 
-    //Regex Testing Methods
-    public static void testRegex(){
-        String[] testStrs = {"quit", "q232uit2", "qekit", "q u i t"};
-        System.out.println("Hello world!");
-       // for(String i : testStrs ){
-        //    quitDetector(i);
-       // }
-        /*System.out.println("Is English letter? a " + isEnglishLetter('a')); //true
-        System.out.println("Is English letter? 8 " + isEnglishLetter('8')); //false
-        System.out.println("Is English letter? A " + isEnglishLetter('A'));//true
-        System.out.println("Is English letter? ! " + isEnglishLetter('!')); //false
-        System.out.println("Is English letter? C " + isEnglishLetter('C')); //true*/
-
-    }
-
-    private static Boolean isEnglishLetter(char inputChar){
-        if (((inputChar >= 'A') && (inputChar <= 'Z')) ||
-                ((inputChar >= 'a') && (inputChar <= 'z'))){
-            return true;
-        }
-        return false;
-    }
-
-    public static Boolean quitDetector(String input){
-        String pattern = "q[\\W]*u[\\W]*i[\\W]*t[\\W]*";
-
-        Pattern patternObj = Pattern.compile(pattern);
-
-        Matcher m = patternObj.matcher(input);
-        if(m.find()){
-            System.out.println("Found value: " + m.group(0));
-            return true;
-        }
-        //Otherwise, we didnt match.
-        System.out.println("No match found!");
-        return false;
+    private Boolean isEnglishLetter(char inputChar) {
+        return ((inputChar >= 'A') && (inputChar <= 'Z')) ||
+                ((inputChar >= 'a') && (inputChar <= 'z'));
     }
 }
