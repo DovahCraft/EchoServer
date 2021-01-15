0686b51723908bcc86e8c2577198c3d54c750d09
Joe Vargovich <josephvargovich@gmail.com>
2021-01-15 09:46:53 -0700

    Early version of quit regex function.

--- src/com/company/EchoServer.java
+++ src/com/company/EchoServer.java
@@ -1,7 +1,33 @@
 package com.company;
+import java.util.regex.*;
+import java.net.*;
+import java.io.*;
+
 
 public class EchoServer {
     public static void main(String[] args){
+        String[] testStrs = {"quit", "quuit2", "qekit", "q u i t"};
         System.out.println("Hello world!");
+        for(String i : testStrs ){
+            quitDetector(i);
+        }
+
+
+
+    }
+
+    public static Boolean quitDetector(String input){
+        String pattern = "q{1}u{1}i{1}t{1}";
+
+        Pattern patternObj = Pattern.compile(pattern);
+
+        Matcher m = patternObj.matcher(input);
+        if(m.find()){
+            System.out.println("Found value: " + m.group(0));
+            return true;
+        }
+        //Otherwise, we didnt match.
+        System.out.println("No match found!");
+        return false;
     }
 }
